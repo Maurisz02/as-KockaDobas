4ff5ec272ab6fcfa5bdd0741761180f372dddbc8
Pinter Adam <p2002adam@gmail.com>
2022-10-13 15:14:03 +0200

    talan kesz

--- app/src/main/java/com/example/kockadobas/MainActivity.java
+++ app/src/main/java/com/example/kockadobas/MainActivity.java
@@ -8,11 +8,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView kocka1, kocka2;
     private Button btn1, btn2, btn3, btn4;
     private TextView szoveg;
+    private Random rnd;
+    private int dobas1;
+    private int dobas2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,90 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (kocka2.getVisibility() == View.GONE){
+                    szoveg.setText(String.valueOf(dobas1));
+                    switch (dobas1)
+                    {
+                        case 1:
+                            kocka1.setImageResource(R.drawable.kocka1);
+                            break;
+                        case 2:
+                            kocka1.setImageResource(R.drawable.kocka2);
+                            break;
+                        case 3:
+                            kocka1.setImageResource(R.drawable.kocka3);
+                            break;
+                        case 4:
+                            kocka1.setImageResource(R.drawable.kocka4);
+                            break;
+                        case 5:
+                            kocka1.setImageResource(R.drawable.kocka5);
+                            break;
+                        case 6:
+                            kocka1.setImageResource(R.drawable.kocka6);
+                            break;
+                    }
+                }else{
+                    int ossz = dobas1+dobas2;
+                    szoveg.setText(String.format("%d (%d + %d)",ossz, dobas1, dobas2));
+
+                    switch (dobas1)
+                    {
+                        case 1:
+                            kocka1.setImageResource(R.drawable.kocka1);
+                            break;
+                        case 2:
+                            kocka1.setImageResource(R.drawable.kocka2);
+                            break;
+                        case 3:
+                            kocka1.setImageResource(R.drawable.kocka3);
+                            break;
+                        case 4:
+                            kocka1.setImageResource(R.drawable.kocka4);
+                            break;
+                        case 5:
+                            kocka1.setImageResource(R.drawable.kocka5);
+                            break;
+                        case 6:
+                            kocka1.setImageResource(R.drawable.kocka6);
+                            break;
+                    }
+
+                    switch (dobas2)
+                    {
+                        case 1:
+                            kocka1.setImageResource(R.drawable.kocka1);
+                            break;
+                        case 2:
+                            kocka1.setImageResource(R.drawable.kocka2);
+                            break;
+                        case 3:
+                            kocka1.setImageResource(R.drawable.kocka3);
+                            break;
+                        case 4:
+                            kocka1.setImageResource(R.drawable.kocka4);
+                            break;
+                        case 5:
+                            kocka1.setImageResource(R.drawable.kocka5);
+                            break;
+                        case 6:
+                            kocka1.setImageResource(R.drawable.kocka6);
+                            break;
+                    }
+                }
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szoveg.setText("");
+            }
+        });
+
     }
 
     private void init(){
@@ -48,5 +137,9 @@ public class MainActivity extends AppCompatActivity {
         btn3 = findViewById(R.id.k3Btn);
         btn4 = findViewById(R.id.k4Btn);
         szoveg = findViewById(R.id.szoveg);
+        rnd = new Random();
+        dobas1 = rnd.nextInt(6)+1;
+        dobas2 = rnd.nextInt(6)+1;
+
     }
 }
